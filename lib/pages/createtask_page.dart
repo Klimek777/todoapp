2ceb78bcf82c971e9a2cfb8e812bc86a15b9f7a0
Dawid Klimowicz <dawidkl777@gmail.com>
2023-07-10 17:03:04 +0200

    init commit

---
+++ lib/pages/createtask_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CreateTaskPage extends StatefulWidget {
+  const CreateTaskPage({super.key});
+
+  @override
+  State<CreateTaskPage> createState() => _CreateTaskPageState();
+}
+
+class _CreateTaskPageState extends State<CreateTaskPage> {
+  double? _deviceHeight, _deviceWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold();
+  }
+}
